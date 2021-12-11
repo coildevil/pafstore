050e36f7990b73ccfb16830586c08b151ecb60ea
LoveOken <cmeamet@protonmail.com>
2021-12-10 23:50:50 -0300

    Add files via upload

--- scripts/products.js
+++ scripts/products.js
@@ -1,44 +1,42 @@
-// utiliza una llave de google sheets api
-const spreadsheetID = '1GSCROoFV3iOp1TGyvzIqc0CBgAZZQ2JLhVUt7cC-5qQ'; // url de la hoja
-const sheetName = 'PAF'; // nombre de la hoja
-const apiKey = 'AIzaSyCPblnS7aZmqPt9nD9Hk0nyx4NDUqA-w-g'; // llave de google api
+(function() {
+    // Utiliza Google Sheets API
+    const sheetName = 'PAF'; // Nombre de Sheet
+    const spreadsheetID = '1GSCROoFV3iOp1TGyvzIqc0CBgAZZQ2JLhVUt7cC-5qQ'; // URL de Sheet
+    const apiKey = 'AIzaSyCPblnS7aZmqPt9nD9Hk0nyx4NDUqA-w-g'; // Llave de API
 
-const url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetID}/values/${sheetName}?key=${apiKey}`;
+    // Obtiene elemento contenedor
+    const container = document.getElementById('products');
 
-function catalogar(products) {
-    console.log(products)
-
-    // usa append para mostrar el catálogo
-    products.forEach(p => {
-        $("#products").append(
-            `<article class="col-10 col-sm-6 col-md-4 col-lg-3 mx-auto mx-sm-0 mb-2" >
-                <div class="text-center card border-0 bg-light rounded-0 h-100">
-                    <div class="paf-image">
-                        <img class="card-img-top rounded-0" src="${p.Imagen}" alt="${p.Titulo}">
-                    </div>  
-                    <div class='card-body'>
-                        <h3 class="mt-2 text-uppercase">
-                            ${p.Titulo}
-                        </h3>
-                        <big>
-                            ${p.Precio}
-                        </big>
+    // Funcion para catalogar
+    function catalogar(products) {
+        // Por cada elemento del catálogo, añadelo al documento
+        products.forEach(p => {
+            container.innerHTML +=
+                `<article class="col-10 col-sm-6 col-md-4 col-lg-3 mx-auto mx-sm-0 mb-2" >
+                    <div class="text-center card border-0 bg-light rounded-0 h-100">
+                        <div class="paf-image">
+                            <img class="card-img-top rounded-0" src="${p.Imagen}" alt="${p.Titulo}">
+                        </div>  
+                        <div class='card-body'>
+                            <h3 class="mt-2 text-uppercase">
+                                ${p.Titulo}
+                            </h3>
+                            <big>
+                                ${p.Precio}
+                            </big>
+                        </div>
                     </div>
-                </div>
-            </article>`
-        );
-    }); // cierre for each
-}
+                </article>`
+        }); // Cierre bucle
+    }
 
-function init() {
-    fetch(url)
+    // Inicio Fetch
+    fetch(`https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetID}/values/${sheetName}?key=${apiKey}`)
         .then((response) => response.json())
         .then(result => {
-            // usa papaparse para obtener google sheets
+            // Usa PapaParse para obtener Google Sheets
             const { data: products } = Papa.parse(Papa.unparse(result.values), { header: true });
 
             catalogar(products);
-        }) // cierre response
-}
-
-$(document).ready(init);
+        }) // Cierre Fetch
+})();
